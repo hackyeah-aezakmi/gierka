87d5b916898b9873c5567787f432b0902608546a
mbroton <michal.broton.ns@gmail.com>
2024-09-28 18:21:59 +0200

    Add godotenv to main

--- server/go.mod
+++ server/go.mod
@@ -5,7 +5,8 @@ go 1.23.1
 require (
 	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/websocket v1.5.3
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.23
 	github.com/rs/cors v1.11.1
-  github.com/sashabaranov/go-openai v1.31.0
-  github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.31.0
 )
--- server/main.go
+++ server/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"github.com/hackyeah-aezakmi/gierka/transport/http"
-	"github.com/hackyeah-aezakmi/gierka/transport/socket"
 	"log"
 	"os"
+
+	"github.com/hackyeah-aezakmi/gierka/transport/http"
+	"github.com/hackyeah-aezakmi/gierka/transport/socket"
+
+	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	openaiApiKey := os.Getenv("OPENAI_API_KEY")
 	if openaiApiKey == "" {
 		log.Fatal("Environment variable OPENAI_API_KEY is required")
