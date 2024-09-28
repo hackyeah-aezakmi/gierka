d69e17d887476754b1cb232757f0b900e88a2465
wr0belj <jakub@wrobelj.com>
2024-09-28 16:38:49 +0200

    add boilerplate of http server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/hackyeah-aezakmi/gierka
 
 go 1.23.1
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/cors v1.11.1
+)
---
+++ server/transport/http/app_user.go
@@ -0,0 +1,10 @@
+package http
+
+import (
+	"net/http"
+)
+
+// UpdateState update state of the current user
+func (h *Handler) UpdateState(w http.ResponseWriter, r *http.Request) {
+
+}
